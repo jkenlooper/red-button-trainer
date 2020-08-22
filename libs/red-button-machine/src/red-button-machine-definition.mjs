122f144a0f35104d39788b3fbd9e271b83af1372
Jake Hickenlooper <jake@weboftomorrow.com>
2020-08-22 06:56:02 -0600

    Add ready state

--- libs/red-button-machine/src/red-button-machine-definition.mjs
+++ libs/red-button-machine/src/red-button-machine-definition.mjs
@@ -2,19 +2,28 @@ import { assign } from "../node_modules/@xstate/fsm/es/index.js";
 
 const state = Object.freeze({
   up: "up",
+  ready: "ready",
   down: "down",
 });
 
 const action = Object.freeze({
   updateButton: "updateButton",
+  sendError: "sendError",
+  setLights: "setLights",
+  startTimer: "startTimer",
+  stopTimer: "stopTimer",
 });
 
 const event = Object.freeze({
   ButtonClicked: "ButtonClicked",
+  Reset: "Reset",
+  Start: "Start",
 });
 
 const context = Object.freeze({
   id: undefined,
+  responseTime: undefined,
+  lights: [undefined, undefined],
 });
 
 const stateDefinitions = {};
@@ -27,7 +36,13 @@ stateDefinitions[state.up] = {
 stateDefinitions[state.up].on[event.ButtonClicked] = [
   {
     target: state.down,
-    actions: [action.updateButton],
+    actions: [action.updateButton, action.sendError],
+  },
+];
+stateDefinitions[state.up].on[event.Start] = [
+  {
+    target: state.ready,
+    actions: [action.setLights, action.startTimer],
   },
 ];
 
@@ -36,10 +51,28 @@ stateDefinitions[state.down] = {
   on: {},
 };
 
+/*
 stateDefinitions[state.down].on[event.ButtonClicked] = [
+  {
+    target: state.down,
+    actions: [action.updateButton],
+  },
+];
+*/
+stateDefinitions[state.down].on[event.Reset] = [
   {
     target: state.up,
     actions: [action.updateButton],
+  },
+];
+
+stateDefinitions[state.ready] = {
+  on: {},
+};
+stateDefinitions[state.ready].on[event.ButtonClicked] = [
+  {
+    target: state.down,
+    actions: [action.stopTimer, action.updateButton],
   },
 ];
 
--- libs/red-button-machine/src/red-button-machine.spec.mjs
+++ libs/red-button-machine/src/red-button-machine.spec.mjs
@@ -8,19 +8,82 @@ import {
 
 const machine = redButtonMachine;
 
-suite("Button states", () => {
-  test("Initial", () => {
+suite("Initial", function () {
+  test("State is up and has default context.", function () {
     chai.assert.equal(machine.initialState.value, state.up);
-  });
-  test("ButtonClicked when state is up", () => {
-    let nextState = machine.transition(state.up, {
-      type: event.ButtonClicked,
-      /*
-      payload: {
-        id: 1,
-      },
-      */
+    chai.assert.deepEqual(machine.initialState.context, {
+      id: undefined,
+      responseTime: undefined,
+      lights: [undefined, undefined],
     });
+  });
+});
+
+suite("Event ButtonClicked", function () {
+  test("When state is up; transition to down.", function () {
+    let nextState = machine.transition(state.up, event.ButtonClicked);
     chai.assert.equal(nextState.value, state.down);
+    chai.assert.deepEqual(nextState.actions, [
+      { type: action.updateButton },
+      { type: action.sendError },
+    ]);
+  });
+
+  test("When state is ready; transition to down.", function () {
+    let nextState = machine.transition(state.ready, event.ButtonClicked);
+    chai.assert.equal(nextState.value, state.down);
+    chai.assert.deepEqual(nextState.actions, [
+      { type: action.stopTimer },
+      { type: action.updateButton },
+    ]);
+  });
+
+  test("When state is down; transition to down.", function () {
+    let nextState = machine.transition(state.down, event.ButtonClicked);
+    chai.assert.equal(nextState.value, state.down);
+    chai.assert.deepEqual(nextState.actions, []);
+  });
+});
+
+suite("Event Reset", function () {
+  test("When state is up; transition to up.", function () {
+    let nextState = machine.transition(state.up, event.Reset);
+    chai.assert.equal(nextState.value, state.up);
+    chai.assert.deepEqual(nextState.actions, []);
+  });
+
+  test("When state is ready; transition to ready.", function () {
+    let nextState = machine.transition(state.ready, event.Reset);
+    chai.assert.equal(nextState.value, state.ready);
+    chai.assert.deepEqual(nextState.actions, []);
+  });
+
+  test("When state is down; transition to up.", function () {
+    let nextState = machine.transition(state.down, event.Reset);
+    chai.assert.equal(nextState.value, state.up);
+    chai.assert.deepEqual(nextState.actions, [{ type: action.updateButton }]);
+  });
+});
+
+suite("Event Start", function () {
+  test("When state is up; transition to ready.", function () {
+    let nextState = machine.transition(state.up, event.Start);
+    chai.assert.equal(nextState.value, state.ready);
+    chai.assert.deepEqual(nextState.actions, [
+      { type: action.setLights },
+      { type: action.startTimer },
+    ]);
+  });
+
+  test("When state is ready; transition to ready.", function () {
+    let nextState = machine.transition(state.ready, event.Start);
+    chai.assert.equal(nextState.value, state.ready);
+    chai.assert.deepEqual(nextState.actions, []);
+  });
+
+  test("When state is down; transition to down.", function () {
+    let nextState = machine.transition(state.down, event.Start);
+    chai.assert.equal(nextState.value, state.down);
+    chai.assert.deepEqual(nextState.actions, []);
   });
 });
