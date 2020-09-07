50e5cec19a5e9b8dee10f8d32de71488793948b6
Jake Hickenlooper <jake@weboftomorrow.com>
2020-09-07 07:33:49 -0600

    Use wokwi elements for components
    Work in progress of switching out custom CSS for already built
    components from the wokwi elements package.

--- libs/red-button-trainer-components/_postinstall.sh
+++ libs/red-button-trainer-components/_postinstall.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail -o errexit;
 
-mkdir -p dist;
-cp node_modules/utility-redbutton-css/utility-redbutton.css \
-  node_modules/suitcss-utils-display/lib/display.css \
+mkdir -p dist/;
+
+cp -r node_modules/@webcomponents/webcomponentsjs \
   dist/;
