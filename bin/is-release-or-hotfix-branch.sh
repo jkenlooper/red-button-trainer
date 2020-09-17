cacb7eacf5ec1d406a857eb0443ab4217fa245c8
Jake Hickenlooper <jake@weboftomorrow.com>
2020-09-17 06:06:11 -0600

    Fix script to check branch name
    Piping to grep is much simpler.

--- bin/is-release-or-hotfix-branch.sh
+++ bin/is-release-or-hotfix-branch.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-currentbranch=$(git for-each-ref --points-at=HEAD --format='%(refname)');
-echo "Verifying that current branch at HEAD matches release/* or hotfix/*";
-echo $currentbranch
-[[ ${currentbranch#refs/heads/} == release/* ]] || [[ ${currentbranch#refs/heads/} == hotfix/* ]]
+echo "Verifying that any branch name at HEAD matches release/* or hotfix/*";
+git for-each-ref --points-at=HEAD --format='%(refname)' \
+  | egrep --silent "refs/heads/(release|hotfix)/"
