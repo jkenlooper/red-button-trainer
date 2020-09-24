92db4553d0e6a61d93e14bce42aa1b1e5e91958c
Jake Hickenlooper <jake@weboftomorrow.com>
2020-09-24 05:44:16 -0600

    Update script for branch name verifying

--- bin/is-release-or-hotfix-branch.sh
+++ bin/is-release-or-hotfix-branch.sh
@@ -1,5 +1,42 @@
 #!/usr/bin/env bash
 
-echo "Verifying that any branch name at HEAD matches release/* or hotfix/*";
+set -o pipefail -o errexit -o nounset
+
+function usage {
+  cat <<USAGE
+
+Usage: ${0} [-h]
+
+Options:
+  -h            Show help
+  -v            Verbose
+
+Checks if any branch name at HEAD matches release/* or hotfix/*.
+Will error with 1 if no match.
+
+USAGE
+  exit 0;
+}
+VERBOSE=0
+while getopts ":hv" opt; do
+  case ${opt} in
+    h )
+      usage;
+      ;;
+    v )
+      VERBOSE=1
+      ;;
+    \? )
+      usage;
+      ;;
+  esac;
+done;
+shift "$((OPTIND-1))";
+
+if [ ${VERBOSE} == 1 ]; then
+  set -o verbose
+fi
+
 git for-each-ref --points-at=HEAD --format='%(refname)' \
   | egrep --silent "refs/heads/(release|hotfix)/"
+
